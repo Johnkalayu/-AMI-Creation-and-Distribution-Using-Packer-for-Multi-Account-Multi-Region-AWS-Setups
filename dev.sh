a8265150e897726fe360552f46b08bbbd4375b45
joni-k <johnkalayu@yahoo.com>
2025-02-14 17:09:41 +1100

    add new stage in gitlab ci

---
+++ dev.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo yum update -y && yum install -y git unzip wget curl jq
+
+sudo yum  amazon-linux-extras install java-openjdk11
+
+sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_arm64/amazon-ssm-agent.rpm
+sudo systemctl start amazon-ssm-agent
+sudo systemctl status amazon-ssm-agent
+
+sudo yum install -y https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+sudo systemctl start amazon-cloudwatch-agent
+sudo systemctl status amazon-cloudwatch-agent
+
+wget https://inspector-agent.amazonaws.com/linux/latest/install
+chmod +x install
+
+sudo yum install -y docker 
+sudo systemctl start docker
+sudo systemctl enable docker
